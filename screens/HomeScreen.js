9db6b9202da1578c023845cabd1f38581f2ccdc0
Hilary Ly <hilaryly@gmail.com>
2020-03-27 16:59:51 -0400

    refactor: HomeScreen now uses only react hooks to obtain firestore data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { connect } from 'react-redux';
 // import { fetchUser } from '../src/reducers/user';
 import * as firebase from 'firebase';
-import { useSelector } from 'react-redux';
-import { useFirestoreConnect } from 'react-redux-firebase';
 import DogTile from '../components/DogTile';
 import { dog } from '../constants/dog';
-// import UserProfile from '../components/UserProfile'
 
 const userContext = React.createContext({
    user: null
@@ -36,77 +32,6 @@ export const useAuth = () => {
 
    return state;
 };
-
-// function findUserId(email) {
-//    const [error, setError] = React.useState(false) 
-//    const [loading, setLoading] = React.useState(true) 
-//    const [userId, setId] = React.useState(null)
-//   // initialize our default state
-//   // when the id attribute changes (including mount)
-//   // subscribe to the recipe document and update
-//   // our state when it changes.
-//   React.useEffect(
-//     () => {
-//       const unsubscribe = firebase
-//          .firestore()
-//          .collection('users')
-//          .where('email', '==', email)
-//          .onSnapshot(
-//             doc => {
-//                // console.log('docs', docs);
-//                setLoading(false);
-//                setId(doc.docs[0].id);
-//             },
-//             err => {
-//                setError(err);
-//             }
-//          );
-
-//       // returning the unsubscribe function will ensure that
-//       // we unsubscribe from document changes when our id
-//       // changes to a different value.
-//       return () => unsubscribe()
-//     },
-//     [email]
-//   )
-
-//   return {
-//      error,
-//      loading,
-//      userId
-//   };
-// }
-
-// function getDogs(id) {
-//       const [error, setError] = React.useState(false)
-//       const [loading, setLoading] = React.useState(true)
-//       const [dogs, setDogs] = React.useState([])
-    
-//       React.useEffect(
-//         () => {
-//           const unsubscribe = firebase
-//             .firestore()
-//             .collection('users')
-//             .doc(id)
-//             .collection('dogs') 
-//             .onSnapshot( snapshot => { 
-//                const dogs = [] 
-//                snapshot.forEach(doc => { dogs.push(doc) }) 
-//                setLoading(false) 
-//                setDogs(dogs) 
-//              }, err => { setError(err) } )
-    
-//           return () => unsubscribe()
-//         },
-//         [id]
-//       )
-    
-//       return {
-//         error,
-//         loading,
-//         dogs,
-//       }
-// }
 
 export default function HomeScreen(props) {
 const { initializing, user } = useAuth();
@@ -185,58 +110,23 @@ const { initializing, user } = useAuth();
    }
    
    console.log('user', user.email)
-   // const hello = findUserId(user.email);
-   // console.log(userId);
    console.log(dogs)
-   // console.log(getDogs(hello.userId))
    return (
-      <userContext.Provider value={{ user }}>
+      <View style={styles.container}>
          <Text>
-            Hello,
-            {user.displayName}
+            Welcome to DogGo
+            {/* {user.firstName}!!! */}
          </Text>
-      </userContext.Provider>
-      // <View style={styles.container}>
-      //    <Text>
-      //       Welcome to DogGo
-      //       {/* {user.firstName}!!! */}
-      //    </Text>
-      //    <Text>Doggos collected:</Text>
-      //    {/* <View style={styles.cardChild}>
-      //          {dogs.map(dog => {
-      //             return <DogTile dog={dog} key={dog.id} />;
-      //          })}
-      //       </View> */}
-      //    <Button title='Sign Out' onPress={signOutUser} />
-      // </View>
+         <Text>Doggos collected:</Text>
+         <View style={styles.cardChild}>
+               {dogs.map(dog => {
+                  return <DogTile dog={dog} key={dog.id} />;
+               })}
+            </View>
+         <Button title='Sign Out' onPress={signOutUser} />
+      </View>
    );
-   //    useFirestoreConnect([
-   //       {
-   //          collection: 'users',
-   //          where: [['email', '==', email]],
-   //          storeAs: 'userLoggedIn'
-   //       }
-   //    ]);
-   //    const user = useSelector(
-   //       state => {
-   //          if (email) return state.firestore.ordered['userLoggedIn'][0]
-   //       }
-   //       // state => state.firestore.ordered['userLoggedIn'][0]
-   //    );
-
-   //    console.log(user, user.id);
-   //    useFirestoreConnect([
-   //       {
-   //          collection: 'users',
-   //          doc: user.id,
-   //          subcollections: [{ collection: 'dogs' }],
-   //          storeAs: 'dogsColl'
-   //       }
-   //    ]);
-   //    const dogs = useSelector(state => {
-   //       if (user && user.id) return state.firestore.ordered['dogsColl'];
-   //    });
-   //   console.log(dogs);
+   
 
 }
 
@@ -253,13 +143,3 @@ const styles = StyleSheet.create({
       flexWrap: 'wrap'
    }
 });
-
-// for all users
-//  useFirestoreConnect([
-//     { collection: 'users' } // or 'todos'
-//  ]);
-// const users = useSelector(state => state.firestore.ordered.users);
-
-// for single user
-// useFirestoreConnect([ { collection: 'users', doc: 'Fy5lvEPjAnWlAeP4pfdEUcjvyRD3' } ]);
-// const users = useSelector(state => state.firestore.ordered.users[0])
