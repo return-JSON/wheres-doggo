2f37fc48be16c40a51dd9c5649c44e1cc53c5d97
Hilary Ly <hilaryly@gmail.com>
2020-03-27 18:29:05 -0400

    revert: user reducer back from what it was in last dev commit

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,7 +4,6 @@ let initialState = {};
 
 // action constant
 const GET_USER = 'GET_USER';
-const GET_COLLECTED_DOGS = 'GET_COLLECTED DOGS';
 
 // action creators
 export const getUser = user => {
@@ -13,30 +12,6 @@ export const getUser = user => {
       user
    };
 };
-
-export const getCollectedDogs = dogsArr => {
-   return {
-      type: GET_COLLECTED_DOGS,
-      dogsArr
-   };
-};
-
-// thunk creators
-// export const fetchUser = email => {
-//    return async dispatch => {
-//       db.collection('users')
-//          .where('email', '==', email)
-//          .get()
-//          .then(async querySnapshot => {
-//             await dispatch(
-//                getUser({
-//                   ...querySnapshot.docs[0].data(),
-//                   id: querySnapshot.docs[0].id
-//                })
-//             );
-//          });
-//    };
-// };
 
 export const fetchUser = email => {
    let userObj, userId;
@@ -55,34 +30,11 @@ export const fetchUser = email => {
    };
 };
 
-export const fetchCollectedDogs = userId => {
-   return async dispatch => {
-      console.log('thunk in reducer file hit', userId);
-      const dogsArr = [];
-      db.collection('users')
-         .doc(userId)
-         .collection('dogs')
-         .get()
-         .then(async querySnapshot => {
-            querySnapshot.forEach(doc => {
-               console.log('doc.data()', doc.data())
-               const { breed, imageUrl, location } = doc.data();
-               dogsArr.push({ key: doc.id, doc, breed, imageUrl, location });
-            });
-            console.log('dogsArr', dogsArr);
-            await dispatch(getCollectedDogs(dogsArr));
-         });
-   };
-};
-
 // sub-reducer
 export default (state = initialState, action) => {
    switch (action.type) {
       case GET_USER:
          return action.user;
-      case GET_COLLECTED_DOGS:
-         console.log('reducer hit', state);
-         return { ...state, dogs: action.dogsArr };
       default:
          return state;
    }
