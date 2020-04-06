d48a8ae2d4bd0f0c28d65e138615231ca2c64c72
Hilary Ly <hilaryly@gmail.com>
2020-04-05 20:53:56 -0400

    style: updated data viz screen with more colors, fixed overlapping label issue in pie chart

--- screens/DataViz.js
+++ screens/DataViz.js
@@ -7,8 +7,7 @@ import {
    VictoryAxis,
    VictoryVoronoiContainer,
    VictoryTooltip,
-   VictoryLabel,
-   VictoryTheme
+   VictoryLabel
 } from 'victory-native';
 import { db } from '../config/firebase';
 import {
@@ -17,36 +16,8 @@ import {
    editCity
 } from '../constants/utilityFunctions';
 
-class CustomLabel extends React.Component {
-   render() {
-      return (
-         <g>
-            <VictoryLabel {...this.props} />
-            <VictoryTooltip
-               {...this.props}
-               x={200}
-               y={250}
-               orientation='top'
-               pointerLength={0}
-               cornerRadius={50}
-               flyoutWidth={100}
-               flyoutHeight={100}
-               flyoutStyle={{ fill: 'black' }}
-            />
-         </g>
-      );
-   }
-}
-
-CustomLabel.defaultEvents = VictoryTooltip.defaultEvents;
-
-
-
 export default function DataViz(props) {
    const [allDogs, setAllDogs] = React.useState([]);
-
-
-   
    React.useEffect(() => {
       const unsubscribe = db
          .collectionGroup('userDogs')
@@ -78,77 +49,86 @@ export default function DataViz(props) {
    }, []);
 
    return (
-      <ScrollView style={{ backgroundColor: "#D3E9FF" }}>
+      <ScrollView style={{ backgroundColor: '#D3E9FF' }}>
          <View style={styles.container}>
-         <View style={styles.container}>
-            <Text style={styles.title}>Breeds Found By City/Borough</Text>
-            <VictoryChart
-            theme={VictoryTheme.material}
-            domain={{ x: [0, 4], y: [0, 12] }}
-               height={690}
-               width={450}
-               padding={{ left: 75, right: 100, top: 30, bottom: 150}}
-               containerComponent={<VictoryVoronoiContainer />}
-            >
-               <VictoryAxis
-                  tickLabelComponent={<VictoryLabel angle={90}  />}
-                  style={{
-                     tickLabels: {
-                        padding: 5,
-                        verticalAnchor: 'middle',
-                        textAnchor: 'start'
+            <View style={styles.container}>
+               <Text style={styles.title}>Breeds Found By City/Borough</Text>
+               <VictoryChart
+                  domain={{ x: [0, 4], y: [0, 12] }}
+                  height={690}
+                  width={450}
+                  padding={{ left: 75, right: 100, top: 30, bottom: 150 }}
+                  containerComponent={<VictoryVoronoiContainer />}
+               >
+                  <VictoryAxis
+                     tickLabelComponent={<VictoryLabel angle={90} />}
+                     style={{
+                        tickLabels: {
+                           padding: 5,
+                           verticalAnchor: 'middle',
+                           textAnchor: 'start'
+                        }
+                     }}
+                  />
+                  <VictoryAxis
+                     width={500}
+                     height={500}
+                     tickLabelComponent={<VictoryLabel />}
+                     dependentAxis
+                     style={{
+                        tickLabels: { padding: 4 }
+                     }}
+                  />
+                  <VictoryScatter
+                     sortKey='breed'
+                     sortOrder='descending'
+                     labelComponent={
+                        <VictoryTooltip
+                           activateData={true}
+                           renderInPortal={false}
+                           constrainToVisibleArea={true}
+                        />
                      }
-                  }}
-               />
-               <VictoryAxis
-                  width={500}
-                  height={500}
-                  tickLabelComponent={<VictoryLabel />}
-                  dependentAxis
-                  style={{
-                     tickLabels: { padding: 4 }
-                  }}
-               />
-               <VictoryScatter
-                  sortKey='breed'
-                  labelComponent={
-                     <VictoryTooltip
-                        activateData={true}
-                        renderInPortal={false}
-                     />
-                  }
-                  labels={({ datum }) => `${datum.breed}\n${datum.count}`}
-                  bubbleProperty='count'
-                  maxBubbleSize={10}
-                  minBubbleSize={5}
-                  data={geoBreedFreq(allDogs)}
-                  x='boroughOrCity'
-                  y='breed'
-                  style={{ data: { fill: "navy", width: 12} ,padding:35 }}
-               />
-            </VictoryChart>
+                     labels={({ datum }) => `${datum.breed}\n${datum.count}`}
+                     bubbleProperty='count'
+                     maxBubbleSize={15}
+                     minBubbleSize={8}
+                     data={geoBreedFreq(allDogs)}
+                     x='boroughOrCity'
+                     y='breed'
+                     style={{
+                        data: { fill: '#031A6B', width: 12 },
+                        padding: 35
+                     }}
+                  />
+               </VictoryChart>
             </View>
-
-
 
             <View style={styles.containerIn}>
                <Text style={styles.title}>Total Breed Distribution</Text>
                <VictoryPie
-                  height={400}
+                  height={410}
                   padding={{ top: 85, bottom: 80, left: 65, right: 65 }}
                   data={breedFreq(allDogs)}
                   x='breed'
                   y='count'
-                  sortKey='count'
                   innerRadius={70}
-                  labelRadius={125}
-                  colorScale={['navy','gold']}
+                  labelRadius={140}
+                  colorScale={[
+                     '#031A6B',
+                     '#FFE066',
+                     '#FCC244',
+                     'tomato',
+                     '#247BA0'
+                  ]}
                   labels={({ datum }) => `${datum.breed}\n${datum.count}`}
                   style={{
-                     labels: {fontSize: 12, fill: "#c43a31", padding: 9
-                     },
-                     data: {fillOpacity: 0.9, stroke: "white", strokeWidth:3}}}
-
+                     labels: { fontSize: 12, fill: '#031A6B', padding: 9 },
+                     data: {
+                        fillOpacity: 0.9,
+                        stroke: 'white'
+                     }
+                  }}
                />
             </View>
          </View>
@@ -170,6 +150,6 @@ const styles = StyleSheet.create({
    },
    containerIn: {
       flex: 1,
-      marginTop: 90
+      marginTop: 25
    }
 });
